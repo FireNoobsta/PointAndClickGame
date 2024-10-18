41505ee608486dd34774b921ab52d01f599b9b2e
FireNoobsta <FireNoobsta@gmail.com>
2024-10-18 11:25:09 -0700

    Fix flashlight leaving viewport

--- Assets/Scripts/flash_light_occluder.gd
+++ Assets/Scripts/flash_light_occluder.gd
@@ -8,4 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	self.position = get_global_mouse_position()
+	if get_viewport_rect().has_point(get_viewport().get_mouse_position()):
+		self.position = get_global_mouse_position()
